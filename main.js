72181ebe9986ce7fd9674f099ae3d8a9059c3ef0
Sulca2099 <Lucasaswenson@gmail.com>
2026-03-21 20:49:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function darkModeToggle(){
   const page document.getElementById('bodythe');
+  alert('Dark mode on');
   page.style.color = (page.style.color=='rgb(0,0,0)') ? 'rgb(255,255,255)' : 'rgb(0,0,0)';
   
   return 0;
